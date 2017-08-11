517717f215ec2d541305e923e63ca5bfa5c1bb5a
Drew Keller <drew@eightycolumns.com>
2017-08-11 08:10:12 -0400

    Simplify conditionals with ternary operator

--- checkerboard.js
+++ checkerboard.js
@@ -1,10 +1,6 @@
 for (let i = 0; i < 8; i += 1) {
   for (let j = 0; j < 8; j += 1) {
-    if (is_even(i + j)) {
-      process.stdout.write('*');
-    } else {
-      process.stdout.write(' ');
-    }
+    process.stdout.write(is_even(i + j) ? '*' : ' ');
   }
 
   process.stdout.write('\n');
