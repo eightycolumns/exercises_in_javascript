c5d7128353d4678631f8a4d7e7a883fdfa516319
Drew Keller <drew@eightycolumns.com>
2017-08-11 08:08:24 -0400

    Extract is_even()

--- checkerboard.js
+++ checkerboard.js
@@ -1,6 +1,6 @@
 for (let i = 0; i < 8; i += 1) {
   for (let j = 0; j < 8; j += 1) {
-    if ((i + j) % 2 == 0) {
+    if (is_even(i + j)) {
       process.stdout.write('*');
     } else {
       process.stdout.write(' ');
@@ -8,4 +8,8 @@ for (let i = 0; i < 8; i += 1) {
   }
 
   process.stdout.write('\n');
+}
+
+function is_even(d) {
+  return d % 2 == 0;
 }
