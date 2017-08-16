0420de21a83c1f5c75175122355922d51811db2d
Drew Keller <drew@eightycolumns.com>
2017-08-16 07:35:16 -0400

    Extract integerDivision()

--- convert_integer_to_binary_string.js
+++ convert_integer_to_binary_string.js
@@ -7,6 +7,16 @@ for (let i = 0; i < 256; i += 1) {
   );
 }
 
+function integerDivision(dividend, divisor) {
+  let quotient = dividend / divisor;
+
+  if (quotient < 0) {
+    return Math.ceil(quotient);
+  } else {
+    return Math.floor(quotient);
+  }
+}
+
 function toBinaryString(int) {
   if (int === 0) {
     return '0';
@@ -16,7 +26,7 @@ function toBinaryString(int) {
 
   while (int > 0) {
     binaryString = int % 2 + binaryString;
-    int = Math.floor(int / 2);
+    int = integerDivision(int, 2);
   }
 
   return binaryString;
