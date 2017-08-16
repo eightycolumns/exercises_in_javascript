05d555fa76a295824f3a71b99c5dddee25d1f170
Drew Keller <drew@eightycolumns.com>
2017-08-15 20:03:39 -0400

    Move from snake_case to camelCase

--- checkerboard.js
+++ checkerboard.js
@@ -1,11 +1,11 @@
 for (let i = 0; i < 8; i += 1) {
   for (let j = 0; j < 8; j += 1) {
-    process.stdout.write(is_even(i + j) ? '*' : ' ');
+    process.stdout.write(isEven(i + j) ? '*' : ' ');
   }
 
   process.stdout.write('\n');
 }
 
-function is_even(d) {
+function isEven(d) {
   return d % 2 == 0;
 }
