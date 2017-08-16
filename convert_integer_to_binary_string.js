4597be1e20ed5bc8cd70508f8160ed01778dbf04
Drew Keller <drew@eightycolumns.com>
2017-08-16 06:55:26 -0400

    Extract toBinaryString()

--- convert_integer_to_binary_string.js
+++ convert_integer_to_binary_string.js
@@ -1,16 +1,23 @@
 for (let i = 0; i < 256; i += 1) {
-  let binaryString = '';
-
-  if (i === 0) {
-    binaryString = '0';
-  } else {
-    for (let j = i; j > 0; j = Math.floor(j / 2)) {
-      binaryString = j % 2 + binaryString;
-    }
-  }
+  let binaryString = toBinaryString(i);
 
   console.log(
     ' '.repeat(3 - i.toString().length) + i + '\t' +
     ' '.repeat(8 - binaryString.length) + binaryString
   );
+}
+
+function toBinaryString(int) {
+  if (int === 0) {
+    return '0';
+  }
+
+  let binaryString = '';
+
+  while (int > 0) {
+    binaryString = int % 2 + binaryString;
+    int = Math.floor(int / 2);
+  }
+
+  return binaryString;
 }
